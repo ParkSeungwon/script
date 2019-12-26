9986e6049bd75afe70afbbdc34f29b2cad0c0234
zeta <zezeon1@gmail.com>
2019-12-26 10:48:33 +0900

    2019. 12. 26. (목) 10:48:33 KST autocommit

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,17 @@
+#include<optional>
 #include<iostream>
 using namespace std;
 
+optional<string> f(bool i) {
+	if( i) return "" ;
+	else return {};
+}
 int main() {
 	char c[] = {0x20, 0x0e, 0x74, 0x6c, 0x73, 0x31, 0x33, 0x20, 0x66, 0x69, 0x6e, 0x69, 0x73, 0x68, 0x65, 0x64, 0x00};
 	cout << c << endl;
-	for(char a : c) cout << hex << +a << ':' << a << ',';
+//	for(char a : c) cout << hex << +a << ':' << a << ',';
+
+	if(f(true)) cout << "\"\"" << endl;
+	if(f(false)) cout << "{}" << endl;
 }
 
