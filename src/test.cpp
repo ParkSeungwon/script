e077610675d62bf74d239fb9f154c0d0b26b6977
Zeta <zeta@zeta2374.com>
2020-12-02 19:10:46 +0900

    2020. 12. 02. (수) 19:10:46 KST autocommit

--- src/test.cpp
+++ src/test.cpp
@@ -10,23 +10,16 @@
 #include<complex>
 #include<csignal>
 #include<thread>
+#include<type_traits>
 #include<mutex>
 using namespace std;
 
-const char *p[][2] = {
-	{".html", "text/html"},
-	{".css", "text/css"},
-	{".mp3", "web/audio"}
-};
-mutex mtx;
-int f(int a) {
-	static int k=0;
-	int i;
-	for( i=0; i<a; i++) k += i;
-	lock_guard lock{mtx};
-	cout << k << endl;
-}
-int main() {
-	string s = "a.html";
-	cout << s.substr(s.rfind('.'), 4) << endl;
+int main()
+{
+	string s = "\u1100\u1161\u11a8";
+	if(s != "각") cout << s << " is not 각" << endl;
+	ofstream f{s};
+	f << 'a';
+	s = "\uac01";
+	if(s == "각") cout << s << " is 각" << endl;
 }
